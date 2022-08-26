ef5b176b7e64fae1f15d8ef2d12e6b00e49d1eab
marcos.cammarota@telecall.com <marcos.cammarota@telecall.com>
2022-08-26 18:31:02 -0300

    First commit

---
+++ SalesCommission/Controllers/SalesCommissionController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesCommission.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesCommission.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesCommissionController : ControllerBase
+    {
+        public static List<Sale> listSales = new List<Sale>();
+        public static ResponseCommission commission = new ResponseCommission();
+
+        [HttpGet]
+        public List<Sale> GetSales()
+        {
+            return listSales;
+        }
+
+        [HttpPost]
+        public List<Sale> PostRequest(List<Sale> sales)
+        {
+            List<Sale> lista = new List<Sale>();
+            foreach (var sale in sales)
+            {
+                listSales.Add(sale);
+            }
+
+            lista = sales.OrderBy(s => s.Seller).ThenBy(m => m.Date).GroupBy(d => new { d.Seller, d.Date }).Select(x => x.First()).ToList();
+            return lista;
+        }
+    }
+}
---
+++ SalesCommission/Model/Sale.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesCommission.Model
+{
+    public class Sale
+    {
+        public int Seller { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+    }
+}
